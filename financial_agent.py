f34115a295740d45c5208fc643da720e8e6e1d56
Hirusha99 <hirushanethni2277@gmail.com>
2025-02-15 22:50:49 +0530

    add files

---
+++ financial_agent.py
@@ -0,0 +1,47 @@
+
+from phi.agent import Agent
+from phi.model.groq import Groq
+from phi.tools.yfinance import YFinanceTools
+from phi.tools.duckduckgo import DuckDuckGo
+
+import os
+import openai
+from dotenv import load_dotenv
+load_dotenv()
+openai.api_key=os.getenv("OPENAI_API_KEY")
+
+# Websearch Agent
+web_search_agent = Agent(
+    name="Web Search Agent",
+    role="Search the web for the informations",
+    model=Groq(id="llama-3.2-11b-vision-preview"),
+    tools=[DuckDuckGo()],
+    instructions=["Always include sources"],
+    show_tools_calls=True,
+    markdown=True,
+
+)
+
+# Financial Agent
+finance_agent = Agent(
+    name="Finance AI Agent",
+    model=Groq(id="llama-3.2-11b-vision-preview"),
+    tools=[
+        YFinanceTools(stock_price=True, analyst_recommendations=True,
+                      stock_fundamentals=True, company_news=True),
+    ],
+    instructions=["Use tables to display the data"],
+    show_tool_calls=True,
+    markdown=True,
+)
+
+multi_ai_agent = Agent(
+    team=[web_search_agent, finance_agent],
+    model=Groq(id="llama-3.3-70b-versatile"),
+    instructions=["Always include sources", "Use tables to display the data"],
+    show_tool_calls=True,
+    markdown=True,
+)
+
+#query
+multi_ai_agent.print_response("Summarize analyst recommendation and share the latest news for the NVDA",stream=True)
